c932152baf91b056cb04f208fc3755563f61bf36
Neeraj Doharey <neerajlivewire@gmail.com>
2017-05-30 15:26:20 +0530

    Adds ajax request in stock ticker

---
+++ app/models/stock.rb
@@ -0,0 +1,25 @@
+class Stock < ApplicationRecord
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol)
+  end
+
+  def self.new_form_lookup(ticker_symbol)
+    looked_up_stook = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless looked_up_stook.name
+
+    new_stock = new(ticker: looked_up_stook.symbol, name: looked_up_stook.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+
+  def price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+
+    "Unavailable"
+  end
+end
