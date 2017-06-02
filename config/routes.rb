083c739783d15bd2be1948a64049e5fbfde11138
Neeraj Doharey <neerajlivewire@gmail.com>
2017-06-02 13:28:20 +0530

    Add Friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_profile', to: 'users#my_profile'
   get 'search_stocks', to: 'stocks#search'
+
+  get 'my_friends', to: 'users#my_friends'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
